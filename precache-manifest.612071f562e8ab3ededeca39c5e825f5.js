8c6a252be59b1cfe990d675c7d775c27948a1a6e
Pierre Martin <pierre@occitech.fr>
2020-01-10 22:57:53 +0100

    Updates

---
+++ precache-manifest.612071f562e8ab3ededeca39c5e825f5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c501c41ed903c548d76cc165d086af33",
+    "url": "/planning/index.html"
+  },
+  {
+    "revision": "539c61b03be461c82c9e",
+    "url": "/planning/static/js/0.09a048b8.chunk.js"
+  },
+  {
+    "revision": "c108707e15f56813d010",
+    "url": "/planning/static/js/1.b008fee8.chunk.js"
+  },
+  {
+    "revision": "97636669c8193ace9a3b4f1548fc4d0b",
+    "url": "/planning/static/js/1.b008fee8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "985003016e26e49f2c71",
+    "url": "/planning/static/js/10.f8e6dfdf.chunk.js"
+  },
+  {
+    "revision": "86703f2d14afbad690d2",
+    "url": "/planning/static/js/2.602e5aa3.chunk.js"
+  },
+  {
+    "revision": "ba3d517457ec5d463281",
+    "url": "/planning/static/js/5.93c2460a.chunk.js"
+  },
+  {
+    "revision": "b9f9759b9b1285134e0534121ec7f9b0",
+    "url": "/planning/static/js/5.93c2460a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ba1aadfcc24ff31809f",
+    "url": "/planning/static/js/6.404cb661.chunk.js"
+  },
+  {
+    "revision": "24633bc62e76f0fc14cebaed6bfc6cd9",
+    "url": "/planning/static/js/6.404cb661.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25b2765056fa1849cc72",
+    "url": "/planning/static/js/7.3406deaa.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/planning/static/js/7.3406deaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08e88b069c04430e1ece",
+    "url": "/planning/static/js/8.b698364b.chunk.js"
+  },
+  {
+    "revision": "acdf6dde37f162a0eb1e",
+    "url": "/planning/static/js/9.a9e1fc46.chunk.js"
+  },
+  {
+    "revision": "f31eda5270c534c844ab",
+    "url": "/planning/static/js/main.9b0321a8.chunk.js"
+  },
+  {
+    "revision": "c813a884b55c05efa9e3",
+    "url": "/planning/static/js/runtime-main.c5c53814.js"
+  },
+  {
+    "revision": "e069865efdccbd6c741f6e7820816b02",
+    "url": "/planning/static/media/logo.e069865e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/planning/precache-manifest.ccc7b14c7cac7aabcb755397a192fa27.js"
+  "/planning/precache-manifest.612071f562e8ab3ededeca39c5e825f5.js"
 );
 
 self.addEventListener('message', (event) => {
